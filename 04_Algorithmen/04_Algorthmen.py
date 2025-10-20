cfed650b9ba6c549c69a92defb2722b634f45de8
Carsten <carsten.bauer@gmail.com>
2025-10-20 08:57:09 +0200

    Added section 4 file

---
+++ 04_Algorithmen/04_Algorthmen.py
@@ -0,0 +1,18 @@
+cars = ["Audi", "BMW", "Mercedes", "VW"]
+
+# print(cars)
+counter = 0
+
+for car in cars:
+    counter +=1
+    # print(car) 
+    # print(counter)
+
+digits = list(range(0, 10))
+for digit in digits:
+    print(digit)    
+
+min_d = min(digits)
+max_d = max(digits)
+sum_d = sum(digits)
+print(f"Min: {min_d}, Max: {max_d}, Sum: {sum_d}")
